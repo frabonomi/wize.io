34a6a6aac1ad3db53bcbdd3eb1ff92cd8634e5c5
Francesco Bonomi <frbonomi@gmail.com>
2020-11-30 16:52:38 +0100

    Try Mixpanel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import App from './App.svelte'
 import 'boxicons'
+import mixpanel from 'mixpanel-browser'
 
 const app = new App({
   target: document.body,
@@ -7,5 +8,12 @@ const app = new App({
     name: 'world',
   },
 })
+
+mixpanel.init(
+  '164fd793ee0b9db6cb34b905a4b34cce',
+  { api_host: 'https://api-eu.mixpanel.com' },
+  ''
+)
+mixpanel.track_pageview()
 
 export default app
