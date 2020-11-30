aec8a04e4e3c4c916075bb1fa42f15abccabc6c3
Francesco Bonomi <frbonomi@gmail.com>
2020-11-30 17:21:41 +0100

    Remove mixpanel

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import App from './App.svelte'
 import 'boxicons'
-import mixpanel from 'mixpanel-browser'
 
 const app = new App({
   target: document.body,
@@ -8,11 +7,5 @@ const app = new App({
     name: 'world',
   },
 })
-
-mixpanel.init(
-  '164fd793ee0b9db6cb34b905a4b34cce',
-  { api_host: 'https://api-eu.mixpanel.com' },
-  ''
-)
 
 export default app
